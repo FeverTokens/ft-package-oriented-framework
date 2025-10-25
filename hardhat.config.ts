df1b3b8b186dbf8cbf83c0bf0e16159b35300cfe
Moe <33616541+mtoub@users.noreply.github.com>
2025-10-25 20:59:18 +0200

    Remove unused ftganacheConfig from hardhat config
    Removed ftganacheConfig to simplify configuration.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,13 +5,6 @@ import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-network-helpers";
 import "@nomicfoundation/hardhat-verify";
 
-// import "@typechain/hardhat"; // Intentionally disabled to avoid typechain ABI parsing issue during compile
-
-const ftganacheConfig = {
-  url: "http://a431184bd3f754da4b95e067b1e81ad4-113731396.eu-west-3.elb.amazonaws.com:8545",
-  chainId: 1337,
-  // You might need to add more configuration options here based on your requirements
-};
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
   networks: {},
